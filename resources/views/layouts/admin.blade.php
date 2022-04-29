42be10520456ec4d0150407ddfa5100145d92650
westlove <ignaciowestlove@gmail.com>
2022-04-29 15:52:21 +0100

    sweetalert to admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -9,7 +9,7 @@
 
     <title>{{ config('app.name', 'Gadget') }}</title>
 
-    
+
     <!-- Styles -->
     {{-- icons --}}
     <link href="{{ asset('frontend/admin/css/all.min.css') }}" rel="stylesheet">
@@ -18,26 +18,32 @@
     <link href="{{ asset('frontend/admin/css/argon-dashboard.css?v=1.1.2') }}" rel="stylesheet">
     {{-- fonts --}}
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
-    
+
 </head>
 <body class="">
   @include('layouts.inc.adminsidebar')
   <div class="main-content">
     @include('layouts.inc.adminnav')
-  
+
     {{-- content  --}}
     @yield('content')
    @include('layouts.inc.adminfooter')
   </div>
-    
-    
+
+
     </main>
      <!-- Scripts -->
-    
+
      <script src="{{ asset('frontend/admin/js/jquery.min.js') }}" defer></script>
      <script src="{{ asset('frontend/admin/js/bootstrap.bundle.min.js') }}" defer></script>
      <script src="{{ asset('frontend/admin/js/argon-dashboard.min.js?v=1.1.2') }}" defer></script>
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+@if (session('Status'))
+<script>
+swal("{{session('Status')}}", "info");
+</script>
+@endif
      @yield('script')
-     
+
 </body>
 </html>
